f449670b23ce98ce7acac5ac79e65f0f646b54cd
kodongwon <dw9706@naver.com>
2024-12-10 14:50:13 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/gitaction_practice/GitActionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.gitaction_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/gitaction_practice/GitActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.gitaction_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
