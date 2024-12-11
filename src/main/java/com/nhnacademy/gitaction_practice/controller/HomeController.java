edac844c1fe204b360e75738098a3113597870b1
kodongwon <dw9706@naver.com>
2024-12-11 12:29:06 +0900

    t

--- src/main/java/com/nhnacademy/gitaction_practice/controller/HomeController.java
+++ src/main/java/com/nhnacademy/gitaction_practice/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("word", "hello word! It's version 33333");
+        model.addAttribute("word", "hello word! It's version rollling");
         return "index";
     }
 }
